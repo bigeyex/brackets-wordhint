c70462ed1c64a1ca6cacab3406e28231e270b522
ShouheiHayashi <ShouheiHayashi@users.noreply.github.com>
2016-09-12 17:28:33 +0800

    Update main.js
    ignore case

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ define(function (require, exports, module) {
             var rawWordList = editor.document.getText().match(this.tokenDefinition);
             this.cachedWordList = [];
             for(var i in rawWordList){
-               var word = rawWordList[i]; if(this.cachedWordList.indexOf(word)==-1){
+               var word = rawWordList[i]; if(this.cachedWordList.toLowerCase().indexOf(word.toLowerCase())==-1){
                    this.cachedWordList.push(word);
                }
             }
@@ -97,7 +97,7 @@ define(function (require, exports, module) {
         if(symbolBeforeCursorArray){
             // find if the half-word inputed is in the list
             for(var i in this.cachedWordList){
-                if(this.cachedWordList[i].indexOf(symbolBeforeCursorArray[0])==0){
+                if(this.cachedWordList[i].toLowerCase().indexOf(symbolBeforeCursorArray[0].toLowerCase())==0){
                     return true;  
                 }
             }
@@ -140,7 +140,7 @@ define(function (require, exports, module) {
         if(symbolBeforeCursorArray === null) return null;
         if(cachedWordList === null) return null;
         for(var i in this.cachedWordList){
-            if(this.cachedWordList[i].indexOf(symbolBeforeCursorArray[0])==0){
+            if(this.cachedWordList[i].toLowerCase().indexOf(symbolBeforeCursorArray[0].toLowerCase())==0){
                 hintList.push(this.cachedWordList[i]);
             }
         }
